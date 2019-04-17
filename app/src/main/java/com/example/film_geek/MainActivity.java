785f2a5edba8932f48faec8de1b3a9fe47e97138
vali201232 <v.kellermair@gmail.com>
2019-04-17 10:22:02 +0200

    Register

--- app/src/main/java/com/example/film_geek/MainActivity.java
+++ app/src/main/java/com/example/film_geek/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.film_geek;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     EditText emailText;
     EditText passwordText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +28,33 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         emailText = findViewById(R.id.emailText);
         passwordText = findViewById(R.id.passwordText);
+        mAuth = FirebaseAuth.getInstance();
 
-        
+        register(emailText.getText().toString(), passwordText.getText().toString());
+    }
+
+    public void register(String email, String password) {
+        final String TAG = "Method: register";
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+
+
+                        }
+
+                        // ...
+                    }
+                });
 
     }
+
+
 }
