786461d43f3c62300d6a2b78ee4a72601a5c1439
vali201232 <v.kellermair@gmail.com>
2019-04-17 09:57:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/film_geek/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.film_geek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText emailText;
+    EditText passwordText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+
+        
+
+    }
+}
